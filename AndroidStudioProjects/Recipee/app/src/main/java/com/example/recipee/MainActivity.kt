c27962dfb12a4ff199d86cea4478664b0caebdbc
Madcamp2 <navygrace8389@gmail.com>
2025-01-13 16:56:23 +0900

    주황

--- AndroidStudioProjects/Recipee/app/src/main/java/com/example/recipee/MainActivity.kt
+++ AndroidStudioProjects/Recipee/app/src/main/java/com/example/recipee/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.recipee
 
+import android.content.res.ColorStateList
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -13,6 +15,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        // 네비게이션 아이콘 색상 설정
+        bottomNavigation.itemIconTintList = ColorStateList(
+            arrayOf(
+                intArrayOf(android.R.attr.state_checked),
+                intArrayOf(-android.R.attr.state_checked)
+            ),
+            intArrayOf(
+                ContextCompat.getColor(this, R.color.selected_color),
+                ContextCompat.getColor(this, R.color.unselected_color)
+            )
+        )
+
         bottomNavigation.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.navigation_home -> {
